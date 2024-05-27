ef511c1c336392a7297eb2481181d76e12949a92
mariaabotelho <168577465+mariaabotelho@users.noreply.github.com>
2024-05-27 03:47:54 -0300

    Update hungryy.py

--- hungryy.py
+++ hungryy.py
@@ -6,16 +6,13 @@ from streamlit_folium import st_folium
 from streamlit_custom_notification_box import custom_notification_box
 from PIL import Image
 import numpy as np
-
 # URL direta para a imagem do Cristo no GitHub
 image_url = "https://raw.githubusercontent.com/mariaabotelho/rio.eats/main/cristinho%202.jpg"
 profile_image_url = "https://raw.githubusercontent.com/mariaabotelho/rio.eats/main/matheuss.jpg"
 logo_url = "https://raw.githubusercontent.com/mariaabotelho/rio.eats/main/rio%20eats.jpg"
-
 # Espaço para armazenar as imagens capturadas
 if 'captured_images' not in st.session_state:
     st.session_state.captured_images = []
-
 # Função para exibir o perfil fake
 def mostrar_perfil():
     st.markdown(
@@ -27,26 +24,9 @@ def mostrar_perfil():
         """,
         unsafe_allow_html=True
     )
-    st.markdown(
-        f"""
-        <div style="position: relative; display: inline-block;">
-            <span>Nível Bronze</span>
-            <span style="visibility: hidden; width: 200px; background-color: #f9f9f9; color: #333; text-align: center; border-radius: 5px; padding: 5px 0; position: absolute; z-index: 1; bottom: 125%; left: 50%; margin-left: -100px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);">
-                Nível Bronze: Esse usuário já avaliou entre 1 e 20 restaurantes.
-            </span>
-        </div>
-        <style>
-            div:hover > span:last-child {
-                visibility: visible;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
     st.write("""
         Professor de Ciência de Dados durante o dia, explorador de butecos durante a noite. Entre algoritmos e cervejas geladas, eu desvendo os mistérios dos dados e dos petiscos de boteco. Se você quer discutir sobre machine learning ou descobrir o melhor pastel de feira, sou a pessoa certa! No meu tempo livre, estou sempre em busca do próximo buteco perfeito, onde a comida é boa, a cerveja é gelada e a conversa é animada. Vamos juntos nessa jornada gastronômica?
     """)
-
     # Tabs para separar as seções
     tab1, tab2, tab3 = st.tabs(["Fotos de Pratos", "Top 5 Restaurantes", "Interações"])
     
@@ -66,7 +46,7 @@ def mostrar_perfil():
             st.image("casa tua cocina.jpg", use_column_width=True)
         
         # Widget para capturar imagem da webcam
-        picture = st.camera_input("Hmm parece estar gostoso.. Tire uma foto da sua comida para registrar!")
+        picture = st.camera_input("HMM PARECE ESTAR GOSTOSO... TIRE UMA FOTO PARA REGISTRAR SUA COMIDA!")
         
         # Botão para salvar a imagem capturada
         if picture:
@@ -78,7 +58,6 @@ def mostrar_perfil():
             st.subheader("Imagens Capturadas")
             for img in st.session_state.captured_images:
                 st.image(img)
-
     with tab2:
         st.subheader("Top 5 Restaurantes")
         st.write("1. Irajá Redux")
@@ -86,7 +65,6 @@ def mostrar_perfil():
         st.write("3. Mocellin Steakhouse")
         st.write("4. Casa Tua Cocina")
         st.write("5. Paris 6")
-
     with tab3:
         st.subheader("Interações")
         interacoes = [
@@ -105,7 +83,6 @@ def mostrar_perfil():
                 """,
                 unsafe_allow_html=True
             )
-
 # Função para exibir notificações temporárias
 def exibir_notificacao():
     styles = {
@@ -126,20 +103,22 @@ def exibir_notificacao():
 
 # Barra lateral
 with st.sidebar:
+     # Link para a seção de perfil
+    pagina = st.selectbox("Navegação", ["Mapa", "Perfil"])
     st.image(logo_url, use_column_width=True)
     st.header('Rio Eats')
-    st.write('O site que vai ajudar a achar o restaurante mais pertinho de você')
-    st.caption('Criado por Luaninha, Julinha e Mary')
-    
+    st.write('O site que conecta amantes da gastronomia de forma moderna e interativa. Encontre restaurantes próximos, registre suas visitas, compartilhe experiências e descubra novos lugares recomendados pela comunidade. Transforme cada refeição em uma aventura gastronômica personalizada.')
+    st.caption('Criado por Maria Botelho, Julia Frazão e Luana Pinheiro')
+
     # Link para a seção de perfil
     pagina = st.selectbox("Navegação", ["Mapa", "Perfil"])
+
 
 if pagina == "Perfil":
     mostrar_perfil()
 else:
     # Exibir a notificação personalizada no início da página
     exibir_notificacao()
-
     # Título com a imagem ao lado
     st.markdown(
         f"""
@@ -150,34 +129,26 @@ else:
         """,
         unsafe_allow_html=True
     )
-
     st.write('O Rio Eats chegou para deixar mais fácil a sua escolha de restaurante na cidade maravilhosa!')
-
     # Carregar dados diretamente do CSV limpo com estrelas
     data = pd.read_csv('restaurantes_final_limpo_com_estrelas.csv')
-
     # Criar um filtro para o tipo de culinária
     opcoes_culinaria = data['CULINARIA'].unique()
     culinaria_selecionada = st.multiselect('Selecione Tipos de Culinária', opcoes_culinaria, default=opcoes_culinaria[:3])
-
     # Filtrar dados com base nos tipos de culinária selecionados
     dados_filtrados = data[data['CULINARIA'].isin(culinaria_selecionada)]
-
     # Criar mapa
     m = folium.Map(location=[dados_filtrados['latitude'].mean(), dados_filtrados['longitude'].mean()], zoom_start=12)
     marker_cluster = MarkerCluster().add_to(m)
-
     # Adicionar marcadores ao mapa
     for idx, row in dados_filtrados.iterrows():
         folium.Marker(location=[row['latitude'], row['longitude']],
                       popup=f"{row['NOME']} - {row['CULINARIA']}",
                       icon=folium.Icon(color="blue", icon="info-sign")).add_to(marker_cluster)
-
     # Exibir mapa
     st_folium(m, width=700, height=500)
-
     # Exibe info dos restaurantes
     for idx, row in dados_filtrados.iterrows():
         with st.expander(row['NOME']):
-            st.markdown(f"**Endereço**: {row['ENDERECO']}")
-            st.markdown(f"**Estrelas**: {'⭐' * row['estrelas']}")
+            st.markdown(f"*Endereço*: {row['ENDERECO']}")
+            st.markdown(f"*Estrelas*: {'⭐' * row['estrelas']}")
